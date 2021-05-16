562370efbad55587b915d371fa64c02b55d41eeb
Felix <felix.laed@gmail.com>
2021-05-16 10:33:26 +0200

    Update newTOV.F90
    add possibility for entropy slice

--- src/newTOV.F90
+++ src/newTOV.F90
@@ -14,7 +14,6 @@ PROGRAM newTOV
 ! The code is build upon better understanding the equations and process
 ! of building neutron star models. For sure it can be made much faster 
 ! and efficient. 
-! Feel free to commit and pull requests...
 
 	USE eosmodule
 	IMPLICIT NONE
@@ -24,7 +23,7 @@ PROGRAM newTOV
 	CHARACTER*50 :: filename
 
 	! variables for EOSDriver
-	INTEGER keytemp,keyerr,TOV
+	INTEGER keytemp,keyerr,TOV,points,i
 	DOUBLE PRECISION xrho,xye,xtemp,xtemp2
 	DOUBLE PRECISION xenr,xprs,xent,xcs2,xdedt,xmunu
 	DOUBLE PRECISION xdpderho, xdpdrhoe
@@ -34,31 +33,35 @@ PROGRAM newTOV
 	DOUBLE PRECISION rhoc,dmutot,h
 	DOUBLE PRECISION,DIMENSION(4) :: Y
 	DOUBLE PRECISION idens,imass,irho,irad,ipress,iye,itemp
+	DOUBLE PRECISION rhoc_min,rhoc_max,step
 
 !----INPUT VALUES ------------------------------------------------------
 
-	EOStab="LS220.h5"  			! path to EOS table
-	
+	EOStab="LS220.h5"  		! path to EOS table
 	
 	filename="1D_star_profile"	! output filename
 	
 	TOV=0	! 0 - use Newtonian stellar equations
-			! 1 - use TOV equations (GR)
+		    ! 1 - use TOV equations (GR)
 
-	itemp=0.5d0			! temperature (isothermal) in MeV
-	rhoc=0.4893269d15	! central density in cgs
-	h=75.d0				! stepsize for RK in cm		
+	itemp=0.5d0      ! temperature T in MeV
+	xent=1.0d0       ! entropy S in kB
+	keytemp=1        ! 1 - for temperature T-slice, input: rho,T,Ye
+	                 ! 2 - for entropy S-slice, input: rho,S,Ye
+	rhoc=0.4893269d15	 ! central density rho_c in cgs
+	!rho_c=0.4685d15 for entropy slice with S=1.0d0 kbar -> M=1.4 M_sun
+	h=75.d0		     ! stepsize for RK in cm		
 
 !-----------------------------------------------------------------------    
 
 	! initial conditions
 	call readtable(EOStab)
-	call betaeqYe(rhoc,itemp,iye,xprs,dmutot,xenr)
+	call betaeqYe(rhoc,itemp,iye,xprs,dmutot,xenr,xent,keytemp)
 
-	irad=0.d0	! radius				r
-	irho=rhoc	! central density		rhoc
-	Y(1)=0.d0	! baryon mass 			mb 
-	Y(2)=xprs	! central pressure 		Pc
+	irad=0.d0	! radius		r
+	irho=rhoc	! central density	rhoc
+	Y(1)=0.d0	! baryon mass 		mb 
+	Y(2)=xprs	! central pressure 	Pc
 	Y(3)=0.d0	! gravitational mass	mg	[only for full TOV]
 	Y(4)=0.d0	! gravitational pot.	Phi [only for full TOV]
 
@@ -66,7 +69,7 @@ PROGRAM newTOV
 	open(1,file=filename)
 	do while((Y(2).ge.0.0d0).and.(irho.ge.1.5d3)) 
 		write(1,'(7(1x,es23.16))') irad,irho,Y(1),Y(2),itemp,iye,xenr
-		call RK4(irad,irho,itemp,iye,xenr,dmutot,Y,h,TOV)
+		call RK4(irad,irho,itemp,iye,xenr,dmutot,Y,h,TOV,xent,keytemp)
 		irad=irad+h
 	enddo
 	close(1)
@@ -79,7 +82,7 @@ END PROGRAM newTOV
 
 !************************************************************************
 
-SUBROUTINE betaeqYe(rhoe,temp,yee,xprs,dmutot,xenr)
+SUBROUTINE betaeqYe(rhoe,temp,yee,xprs,dmutot,xenr,xent,keytemp)
 
 	! find Ye for beta-equilibrium: dmutot=0=mu_e+mu_p-mu_n
 	! for low densities finds Ye where dmutot is minimal
@@ -95,7 +98,6 @@ SUBROUTINE betaeqYe(rhoe,temp,yee,xprs,dmutot,xenr)
 	DOUBLE PRECISION xabar,xzbar,xmu_e,xmu_n,xmu_p,xmuhat
 	DOUBLE PRECISION xxa,xxh,xxn,xxp
 	
-	keytemp=1		! set EOSDrive input mode to: rho, T, Ye
 	keyerr=0
 	tol=1.0d-9
 	i=0
@@ -124,9 +126,9 @@ SUBROUTINE betaeqYe(rhoe,temp,yee,xprs,dmutot,xenr)
 		
 end SUBROUTINE betaeqYe
 
-SUBROUTINE findrho(press,rho,xtemp,iye,xenr,dmutot)
+SUBROUTINE findrho(press,rho,xtemp,iye,xenr,dmutot,xent,keytemp)
 
-	! find rho for given p, T, Ye with rudimentary bisection
+	! find rho for given p, T, Ye with bisection
 
 	USE eosmodule
 	IMPLICIT NONE
@@ -138,7 +140,6 @@ SUBROUTINE findrho(press,rho,xtemp,iye,xenr,dmutot)
 	DOUBLE PRECISION xdpderho,xdpdrhoe
 	DOUBLE PRECISION xabar,xzbar,xmu_e,xmu_n,xmu_p,xmuhat
 	DOUBLE PRECISION xxa,xxh,xxn,xxp
-	keytemp=1
 	keyerr=0
 	j=0
 	itermax=50
@@ -150,13 +151,7 @@ SUBROUTINE findrho(press,rho,xtemp,iye,xenr,dmutot)
 		xdpderho,xdpdrhoe,xxa,xxh,xxn,xxp,xabar,xzbar,xmu_e,xmu_n,xmu_p,&
 		xmuhat,keytemp,keyerr,precision)
 	do while(abs((xprs-press)/press).ge.tol.and.(j.le.itermax))     
-		call nuc_eos_full(rho,xtemp,iye,xenr,xprs,xent,xcs2,xdedt,&
-			xdpderho,xdpdrhoe,xxa,xxh,xxn,xxp,xabar,xzbar,xmu_e,xmu_n,&
-			xmu_p,xmuhat,keytemp,keyerr,precision)
-		call betaeqYe(rho,xtemp,iye,xprs,dmutot,xenr)
-		call nuc_eos_full(rho,xtemp,iye,xenr,xprs,xent,xcs2,xdedt,&
-			xdpderho,xdpdrhoe,xxa,xxh,xxn,xxp,xabar,xzbar,xmu_e,xmu_n,&
-			xmu_p,xmuhat,keytemp,keyerr,precision)
+		call betaeqYe(rho,xtemp,iye,xprs,dmutot,xenr,xent,keytemp)
 		if(((xprs-press)/press).le.tol)then
 			rhol=rho
 			rho=(rho+rhog)*0.5d0
@@ -164,13 +159,16 @@ SUBROUTINE findrho(press,rho,xtemp,iye,xenr,dmutot)
 			rhog=rho
 			rho=(rho+rhol)*0.5d0 
 		endif
+		call nuc_eos_full(rho,xtemp,iye,xenr,xprs,xent,xcs2,xdedt,&
+			xdpderho,xdpdrhoe,xxa,xxh,xxn,xxp,xabar,xzbar,xmu_e,xmu_n,&
+			xmu_p,xmuhat,keytemp,keyerr,precision)
 		j=j+1
 	enddo
-	call betaeqYe(rho,xtemp,iye,xprs,dmutot,xenr)
+	call betaeqYe(rho,xtemp,iye,xprs,dmutot,xenr,xent,keytemp)
 	
 end SUBROUTINE findrho
 
-SUBROUTINE RK4(irad,irho,itemp,iye,xenr,dmutot,Y,h,TOV)
+SUBROUTINE RK4(irad,irho,itemp,iye,xenr,dmutot,Y,h,TOV,xent,keytemp)
 
 	! Iteration for solving ODE system: dY/dr = f(r,Y) 
 	! with 4th order Runge-Kutta method
@@ -185,56 +183,56 @@ SUBROUTINE RK4(irad,irho,itemp,iye,xenr,dmutot,Y,h,TOV)
 	
 	USE eosmodule
 	IMPLICIT NONE
-	INTEGER TOV
+	INTEGER TOV,keytemp
 	DOUBLE PRECISION, DIMENSION(4) :: Y,iY
 	DOUBLE PRECISION, DIMENSION(4) :: k1,k2,k3,k4
-	DOUBLE PRECISION :: irad,irho,itemp,iye,h,dmutot,xenr
+	DOUBLE PRECISION :: irad,irho,itemp,iye,h,dmutot,xenr,xent
 
 	iY=Y
-	call stellarEq(irad,irho,itemp,iY,Y,TOV)
+	call stellarEq(irad,irho,itemp,iY,Y,TOV,xent,keytemp)
 	k1=h*Y
-	call findrho(iY(2)+k1(2)/2,irho,itemp,iye,xenr,dmutot)
-	call stellarEq(irad+h/2,irho,itemp,iY+k1/2,Y,TOV)
+	call findrho(iY(2)+k1(2)/2,irho,itemp,iye,xenr,dmutot,xent,keytemp)
+	call stellarEq(irad+h/2,irho,itemp,iY+k1/2,Y,TOV,xent,keytemp)
 	k2=h*Y
-	call findrho(iY(2)+k2(2)/2,irho,itemp,iye,xenr,dmutot)
-	call stellarEq(irad+h/2,irho,itemp,iY+k2/2,Y,TOV)
+	call findrho(iY(2)+k2(2)/2,irho,itemp,iye,xenr,dmutot,xent,keytemp)
+	call stellarEq(irad+h/2,irho,itemp,iY+k2/2,Y,TOV,xent,keytemp)
 	k3=h*Y
-	call findrho(iY(2)+k3(2)/2,irho,itemp,iye,xenr,dmutot)
-	call stellarEq(irad+h,irho,itemp,iY+k3,Y,TOV)
+	call findrho(iY(2)+k3(2)/2,irho,itemp,iye,xenr,dmutot,xent,keytemp)
+	call stellarEq(irad+h,irho,itemp,iY+k3,Y,TOV,xent,keytemp)
 	k4=h*Y
 	Y=iY+(k1+2*k2+2*k3+k4)/6
-	call findrho(Y(2),irho,itemp,iye,xenr,dmutot)
+	call findrho(Y(2),irho,itemp,iye,xenr,dmutot,xent,keytemp)
 
 end SUBROUTINE RK4
 
-SUBROUTINE stellarEq(irad,irho,itemp,iY,Y,TOV)
+SUBROUTINE stellarEq(irad,irho,itemp,iY,Y,TOV,xent,keytemp)
 
 	! ODE system for stellar structure: dY/dr = f(r,Y)
-	! with Y(1) mass		m   
-	!			baryon mass	mb  [if TOV]
-	!      Y(2) pressure 	P
-	!      Y(3) grav. mass	mg  [if TOV]
-	!	   Y(4) potential   Phi [if TOV]
+	! with Y(1) mass	 m   
+	!	        baryon mass mb  [if TOV]
+	!      Y(2) pressure 	 P
+	!      Y(3) grav. mass	 mg  [if TOV]
+	!      Y(4) potential   Phi [if TOV]
 	
 	USE eosmodule
 	IMPLICIT NONE
-	INTEGER TOV
+	INTEGER TOV,keytemp
 	DOUBLE PRECISION,DIMENSION(4) :: iY,Y
 	DOUBLE PRECISION irad,irho,c2,slant
-	DOUBLE PRECISION itemp,xenr,iye,dmutot
+	DOUBLE PRECISION itemp,xenr,iye,dmutot,xent
 
     c2=clight*clight
     
     if(TOV.eq.0)then
 		if(iY(1).eq.0.0d0)then
 			Y(1) = 4.d0/3*pi*irho*irad**2
-			Y(2) = -ggrav*irho*8.d0/3*pi*irad
+			Y(2) = -2.d0/3*ggrav*irho**2*pi*irad
 		else
 			Y(1) = 4.d0*pi*irho*irad**2
 			Y(2) = -ggrav*irho*iY(1)/irad**2
 		endif
     else
-		call betaeqYe(irho,itemp,iye,iY(2),dmutot,xenr)
+		call betaeqYe(irho,itemp,iye,iY(2),dmutot,xenr,xent,keytemp)
 		slant=(1.d0+xenr/c2+iY(2)/(irho*c2))
 		if(iY(1).eq.0.0d0)then
 			Y(1) = 4.d0*pi*irho*irad**2
